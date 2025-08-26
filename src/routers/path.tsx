52963bb6f934c3f81949dacc83cbb5774e61b318
akhoa6204 <khoaanh662004@gmail.com>
2025-08-26 09:09:39 +0700

    day5

--- src/routers/path.tsx
+++ src/routers/path.tsx
@@ -9,28 +9,30 @@ import RegisterPage from "../pages/RegisterPage";
 import PrivateRoute from "./Private.route";
 import PublicRoute from "./Public.route";
 import OriginalFormRegister from "../pages/OriginalFormRegister";
+import Day5 from "../pages/Day5";
 
 export const paths: RouteObject[] = [
-  {
-    element: <DefaultLayout />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      {
-        path: "/original-form",
-        element: <OriginalFormRegister />,
-      },
-      {
-        element: <PrivateRoute />,
-        children: [{ path: "favourites", element: <FavouritesPage /> }],
-      },
-      {
-        element: <PublicRoute />,
-        children: [
-          { path: "register", element: <RegisterPage /> },
-          { path: "login", element: <LoginPage /> },
-        ],
-      },
-    ],
-  },
+  // {
+  //   element: <DefaultLayout />,
+  //   children: [
+  //     { path: "/", element: <HomePage /> },
+  //     {
+  //       path: "/original-form",
+  //       element: <OriginalFormRegister />,
+  //     },
+  //     {
+  //       element: <PrivateRoute />,
+  //       children: [{ path: "favourites", element: <FavouritesPage /> }],
+  //     },
+  //     {
+  //       element: <PublicRoute />,
+  //       children: [
+  //         { path: "register", element: <RegisterPage /> },
+  //         { path: "login", element: <LoginPage /> },
+  //       ],
+  //     },
+  //   ],
+  // },
+  { path: "/", element: <Day5 /> },
   { path: "*", element: <NotFoundPage /> },
 ];
